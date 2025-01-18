7518584a58642fb2568d20a11fa095d7e55287b4
Daniel Kletinich <kletinich@gmail.com>
2025-01-18 18:09:01 +0200

    can open chat dashboard by clicking the chat image

--- client/src/main/java/com/client/ChatDashboardController.java
+++ client/src/main/java/com/client/ChatDashboardController.java
@@ -1,5 +1,25 @@
 package com.client;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+/*
+ * A controller for viewing all the open chats, and chat with one of the chats
+ */
 public class ChatDashboardController {
     
+    @FXML
+    private ListView listOfChats;
+
+    @FXML
+    private ScrollPane chatScroller;
+
+    @FXML
+    private VBox chatBox;
+
+    public void initialize(){
+        
+    }
 }
