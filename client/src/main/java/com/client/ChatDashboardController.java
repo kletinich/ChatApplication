c9f8ad405b4b571cb39ce6cb74af6992d20e8c3c
Daniel Kletinich <kletinich@gmail.com>
2025-01-18 18:39:50 +0200

    graphic twiks to the chat box

--- client/src/main/java/com/client/ChatDashboardController.java
+++ client/src/main/java/com/client/ChatDashboardController.java
@@ -3,6 +3,8 @@ package com.client;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
 /*
@@ -17,9 +19,14 @@ public class ChatDashboardController {
     private ScrollPane chatScroller;
 
     @FXML
+    private ImageView search_icon;
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
     private VBox chatBox;
 
     public void initialize(){
-        
     }
 }
