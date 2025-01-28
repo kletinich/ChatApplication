b1246dd7506b333a49bd9ff130d8c9a76826d1eb
Daniel Kletinich <kletinich@gmail.com>
2025-01-28 09:49:41 +0200

    Added validity check and pack functions to register requests

--- CommonClasses/classes/src/main/java/com/classes/RequestPack.java
+++ CommonClasses/classes/src/main/java/com/classes/RequestPack.java
@@ -13,6 +13,14 @@ public abstract class RequestPack {
         try{
             switch(requestCode){
 
+                // user wants to register to the server
+                case Codes.REGISTER_REQUEST:
+                    if(args.length != 4 || !(args[0] instanceof String) || !(args[1] instanceof String || !(args[2] instanceof String) || !(args[3] instanceof String))){
+                        throw new IllegalArgumentException("Invalid arguments sent for login request. Excpected: username - String, password - String, first name - String, last name = String");
+                    } 
+
+                    return packRegisterRequest(requestCode, (String)args[0], (String)args[1], (String)args[2], (String)args[3]);
+
                 // user wants to login to the server
                 case Codes.LOGIN_REQUEST:
                     if(args.length != 2 || !(args[0] instanceof String) || !(args[1] instanceof String)){
@@ -47,8 +55,8 @@ public abstract class RequestPack {
      *                                                        *
      *********************************************************/ 
     
-     // check if a packed general request is valid
-     public static boolean isValidRequest(TreeMap<String, Object> request){
+    // check if a packed general request is valid
+    public static boolean isValidRequest(TreeMap<String, Object> request){
         int requestCode;
         Object check = request.get("request_code");
 
@@ -60,6 +68,9 @@ public abstract class RequestPack {
         requestCode = (int) check;
 
         switch(requestCode){
+            case Codes.REGISTER_REQUEST:
+                return isValidRegisterRequest(request);
+
             case Codes.LOGIN_REQUEST:
                 return isValidLoginRequest(request);
 
@@ -70,10 +81,63 @@ public abstract class RequestPack {
                 System.err.println("Not a valid request");
                 return false;
         }
-     }
+    }
 
-     // check if a packed login request is valid
-     private static boolean isValidLoginRequest(TreeMap<String, Object> request){
+    // check if a packed register request is valid // will be changed and more data will be added
+    private static boolean isValidRegisterRequest(TreeMap<String, Object> request){
+        if(request.size() != 5){
+            System.err.println("Not a valid request");
+            return false;
+        }
+
+        Object check =  request.get("username");
+
+        if(check == null){
+            System.err.println("No username was sent");
+            return false;
+        }
+
+        check = request.get("password");
+
+        if(check == null){
+            System.err.println("No password was sent");
+            return false;
+        }
+
+        if(!(check instanceof String)){
+            System.err.println("Not a valid password");
+            return false;
+        }
+
+        check = request.get("first_name");
+
+        if(check == null){
+            System.err.println("No first name was sent");
+            return false;
+        }
+
+        if(!(check instanceof String)){
+            System.err.println("Not a valid first name");
+            return false;
+        }
+
+        check = request.get("last_name");
+
+        if(check == null){
+            System.err.println("No last name was sent");
+            return false;
+        }
+
+        if(!(check instanceof String)){
+            System.err.println("Not a valid last name");
+            return false;
+        }
+
+        return true;
+    }
+
+    // check if a packed login request is valid
+    private static boolean isValidLoginRequest(TreeMap<String, Object> request){
 
         if(request.size() != 3){
             System.err.println("Not a valid request");
@@ -105,10 +169,10 @@ public abstract class RequestPack {
         }
 
         return true;
-     }
+    }
 
-     // check if a packed get users request is valid
-     private static boolean isValidGetUsersRequest(TreeMap<String, Object> request){
+    // check if a packed get users request is valid
+    private static boolean isValidGetUsersRequest(TreeMap<String, Object> request){
         if(request.size() != 2){
             System.err.println("Not a valid request");
             return false;
@@ -137,6 +201,19 @@ public abstract class RequestPack {
      *                                                        *
      *                                                        *
      *********************************************************/
+
+    // pack a register request
+    private static TreeMap<String, Object> packRegisterRequest(int requestCode, String username, String password, String firstName, String lastName){
+        TreeMap<String, Object> request = new TreeMap<>();
+
+        request.put("request_code", requestCode);
+        request.put("username", username);
+        request.put("password", password);
+        request.put("first_name", firstName);
+        request.put("last_name", lastName);
+
+        return request;
+    }
 
     // Pack a login request
     private  static TreeMap<String, Object> packLoginRequest(int requestCode, String username, String password){
